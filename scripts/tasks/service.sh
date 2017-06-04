c558ea871e568e625a8aba1822cea53128ee03cb
Ahmad N. Raja <ar@penneo.com>
2017-06-04 02:38:20 +0200

    Added command about the service bash script

--- scripts/tasks/service.sh
+++ scripts/tasks/service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# @todo: this script goes away. The entry point is services.js
 
 # ------------
 # Run services
@@ -8,7 +9,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR;
 source ./../lib/text.sh
 
-# @fixme: update the description based on the $COMMAND and the $SERVICE if selected
+h1 "DEPPRECATED"
 h1 "SERVICES : Running penneo services"
 h2 "Starting / Stopping one or more services"
 
