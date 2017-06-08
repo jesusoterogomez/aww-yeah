db56c6e881d653c1933ba4725e6c92c8c3633d01
Ahmad N. Raja <ar@penneo.com>
2017-06-08 21:09:19 +0200

    Get a specific pre-defined service in the list

--- scripts/tasks/list.js
+++ scripts/tasks/list.js
@@ -4,8 +4,16 @@
 var services   = require('./../lib/services');
 var prettyJson = require('prettyjson');
 
+var args    = require('optimist')
+    .usage('List pre-defined services')
+    .argv;
+
 var options = {
   noColor: false
 };
 
-console.log(prettyJson.render(services.getDefined(), options));
+var serviceId = args._[0] || '';
+
+var service = serviceId && services.validate(serviceId);
+
+console.log(prettyJson.render(service || services.getDefined(), options));
