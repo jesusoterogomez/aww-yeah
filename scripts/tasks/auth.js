79b0df9ba284ccd1b36c6998651cbd88b84e1946
Ahmad N. Raja <ar@penneo.com>
2017-06-08 11:06:14 +0200

    Indentation

--- scripts/tasks/auth.js
+++ scripts/tasks/auth.js
@@ -14,20 +14,20 @@ var tokenFile = homeDir + '/.penneo-auth-token-local';
 // Prompt input
 //
 var input = [{
-  name: 'username',
-  default: 'test',
-  required: true
+    name: 'username',
+    default: 'test',
+    required: true
 }, {
-  name: 'password',
-  default: 'test',
-  hidden: true,
-  replace: '*',
-  conform: function (value) {
-    return true;
-  }
+    name: 'password',
+    default: 'test',
+    hidden: true,
+    replace: '*',
+    conform: function (value) {
+        return true;
+    }
 }, {
-  name: 'uri',
-  default: 'http://dev.penneo.com:8002/app_dev.php/api/v1/'
+    name: 'uri',
+    default: 'http://dev.penneo.com:8002/app_dev.php/api/v1/'
 }];
 
 
@@ -35,32 +35,32 @@ prompt.start();
 
 console.log('Please enter your classic credentials to generate an authentication token:');
 prompt.get(input, function (err, result) {
-  var authApi = new Penneo({
-    url: result.uri,
-    auth: 'JWT'
-  });
-
-  return authApi.post('/token/password', {
-    username: result.username,
-    password: result.password
-  }).then(function(res) {
-    write(tokenFile, res.data, function(err) {
-      if (err) {
-        errorHandler(err);
-        return;
-      }
-      console.log("\n");
-      console.log('Token saved in ' + tokenFile + ':');
-      console.log("\n");
-      console.log(res.data);
+    var authApi = new Penneo({
+        url: result.uri,
+        auth: 'JWT'
     });
-  }).catch(errorHandler);
+
+    return authApi.post('/token/password', {
+        username: result.username,
+        password: result.password
+    }).then(function(res) {
+        write(tokenFile, res.data, function(err) {
+            if (err) {
+                errorHandler(err);
+                return;
+            }
+            console.log("\n");
+            console.log('Token saved in ' + tokenFile + ':');
+            console.log("\n");
+            console.log(res.data);
+        });
+    }).catch(errorHandler);
 });
 
 function errorHandler(res) {
-  if (res.error) {
-    console.log(res.error);
-  } else {
-    console.log(res);
-  }
+    if (res.error) {
+        console.log(res.error);
+    } else {
+        console.log(res);
+    }
 }
