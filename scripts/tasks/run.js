450760361db7e17e277a075d7abad578ed9992c2
Ahmad N. Raja <ar@penneo.com>
2017-06-12 00:09:52 +0200

    Starting/stopping selected services (specified as csv)

--- scripts/tasks/run.js
+++ scripts/tasks/run.js
@@ -4,17 +4,19 @@ var args    = require('optimist')
     .usage('Manage a service')
     .argv;
 
-var command   = args._[0] || 'up';
-var serviceId = args._[1] || '';
+var command    = args._[0] || 'up';
+var serviceIds = args._[1] || '';
 
-if (serviceId === 'all') {
+if (serviceIds === 'all') {
     for (var id in services.getDefined()) {
         services.displayInfo(id, true);
         services.exec(id, command);
     }
 } else {
-    services.validate(serviceId);
-    services.displayInfo(serviceId);
-    services.exec(serviceId, command);
+    serviceIds.split(',').forEach((serviceId, index) => {
+        services.validate(serviceId);
+        services.displayInfo(serviceId);
+        services.exec(serviceId, command);
+    });
 }
 
