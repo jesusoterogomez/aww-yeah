46936126c9b3d6769bdd4f5121484133851a92a3
Jesús Otero <jesusoterogomez@gmail.com>
2017-06-16 08:55:08 +0200

    Display environment in `aww defined` output

--- scripts/tasks/defined.js
+++ scripts/tasks/defined.js
@@ -14,9 +14,9 @@ var serviceId = args._[0] || '';
 var service = serviceId && services.validate(serviceId);
 
 // @todo: copied, this should be moved to a common string utils file
-function pad(string) {
-    var chars = '          ';
-    return (chars + string).slice(-chars.length);
+function pad(string, length) {
+    var chars = Array(length).join(' ');
+    return (string + chars).split('').reverse().join('').slice(-chars.length).split('').reverse().join(''); // really? 
 }
 
 if (service) {
@@ -25,17 +25,17 @@ if (service) {
     var all = services.getDefined(true);
     for (var id in all) {
         var service = all[id],
-            name  = service.name,
+            name  = pad(service.name, 22),
             color = 'green',
-            str   = '%s : %s',
-            id    = pad(id);
+            str   = '%s : %s [%s]',
+            id    = pad(id, 10);
         if (!service.enabled) {
             color = 'gray';
             str   = str[color];
         } else {
             id    = id['green'];
         }
-        console.log(str, id, name);
+        console.log(str, id, name, service.environment);
     }
 }
 
